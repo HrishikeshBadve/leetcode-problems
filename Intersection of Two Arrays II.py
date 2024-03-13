2e30497a5eb6866df588442206388d8a97f5f261
Hrishikesh Badve <112002709+HrishikeshBadve@users.noreply.github.com>
2024-03-13 16:39:56 -0500

    Update Intersection of Two Arrays II.py

--- Intersection of Two Arrays II.py
+++ Intersection of Two Arrays II.py
@@ -6,19 +6,11 @@ class Solution(object):
         :rtype: List[int]
         """
         res=[]
-        if (len(nums1)<len(nums2)):
-            for i in nums1:
-                for j in nums2:
-                    if(i==j):
-                        res.append(i)
-                        nums2.remove(j)
-                        break
-        else:
-            nums2set=set(nums2)
-            for i in nums2:
-                for j in nums1:
-                    if(i==j):
-                        res.append(i)
-                        nums1.remove(j)
-                        break
+        for i in nums1:
+            for j in nums2:
+                if(i==j):
+                    res.append(i)
+                    nums2.remove(j)
+                    break
+        
         return res
